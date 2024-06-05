8b0cc73bbc6e1ce7e4d88e4cd197b8b5eca17f58
Tran Ha Khuong Duy <khuongduy.tech@gmail.com>
2024-06-05 22:58:47 +0700

    add method PUT(Update)

--- web-api-example/Controller/StockController.cs
+++ web-api-example/Controller/StockController.cs
@@ -49,5 +49,27 @@ namespace web_api_examlpe.Controller
             return CreatedAtAction(nameof(GetbyID), new { id = stockModel.Id}, stockModel.ToStockDto());
         }
 
+        [HttpPut]
+        [Route("{id}") ]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto) 
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+
+            if (stockModel == null) {
+
+                return NotFound();
+            }
+
+            stockModel.Symbol = updateDto.Symbol;
+            stockModel.CompanyName = updateDto.CompanyName;
+            stockModel.Purchase = updateDto.Purchase;
+            stockModel.LastDiv = updateDto.LastDiv;
+            stockModel.Industry = updateDto.Industry;
+            stockModel.MarketCap = updateDto.MarketCap;
+
+            _context.SaveChanges();
+
+            return Ok(stockModel.ToStockDto());
+        }
     }   
 }
