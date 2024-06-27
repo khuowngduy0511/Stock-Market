e955f68fbe90f6835cee38d7e143f3e167373af9
Tran Ha Khuong Duy <khuongduy.tech@gmail.com>
2024-06-27 23:41:28 +0700

    feat: Portfolio GET

---
+++ web-api-example/Controller/PortfolioController .cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using web_api_examlpe.Extensions;
+using web_api_examlpe.Interfaces;
+using web_api_examlpe.Models;
+
+namespace web_api_examlpe.Controller
+{
+    [Route("api/portfolio")]
+    [ApiController]
+    public class PortfolioController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IStockRepository _stockRepo;
+        private readonly IPortfolioRepository _portfolioRepo;
+        public PortfolioController (UserManager<AppUser> userManager, IStockRepository stockRepo, 
+        IPortfolioRepository portfolioRepo)
+        {
+            _userManager = userManager;
+            _stockRepo = stockRepo; 
+            _portfolioRepo = portfolioRepo;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetUserPortfolio()
+        {
+            var username = User.GetUsername();
+            var appUser =  await _userManager.FindByNameAsync(username);
+            var userPortfolio = await _portfolioRepo.GetUserPortfolio(appUser);
+            return Ok(userPortfolio);
+        }
+    }
+}
---
+++ web-api-example/Interfaces/IPortfolioRepository.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using web_api_examlpe.Models;
+
+namespace web_api_examlpe.Interfaces
+{
+    public interface IPortfolioRepository
+    {
+        Task<List<Stock>> GetUserPortfolio(AppUser user);
+    }
+}
