938ff24d49dad2f19bd23233d9c4cb3379c3eb1d
Tran Ha Khuong Duy <khuongduy.tech@gmail.com>
2024-07-09 08:35:15 +0700

    feature: Portfolio DELETE

--- web-api-example/Controller/PortfolioController .cs
+++ web-api-example/Controller/PortfolioController .cs
@@ -57,7 +57,7 @@ namespace web_api_examlpe.Controller
             };
 
             await _portfolioRepo.CreateAsync(portfolioModel);
-            
+
             if (portfolioModel == null)
             {
                 return StatusCode(500, "Could not create");
@@ -66,6 +66,29 @@ namespace web_api_examlpe.Controller
             {
                 return Created();
             }
+        }
+
+        [HttpDelete]
+        [Authorize]
+        public async Task<IActionResult> DeletePortfolio(string symbol)
+        {
+            var username = User.GetUsername();
+            var appUser = await _userManager.FindByNameAsync(username);
+
+            var userPortfolio = await _portfolioRepo.GetUserPortfolio(appUser);
+
+            var filteredStock = userPortfolio.Where(s => s.Symbol.ToLower() == symbol.ToLower()).ToList();
+
+            if (filteredStock.Count() == 1)
+            {
+                await _portfolioRepo.DeletePortfolio(appUser, symbol);
+            }
+            else
+            {
+                return BadRequest("Stock not in your portfolio");
+            }
+
+            return Ok();
         }
     }
 }
--- web-api-example/Interfaces/IPortfolioRepository.cs
+++ web-api-example/Interfaces/IPortfolioRepository.cs
@@ -10,5 +10,6 @@ namespace web_api_examlpe.Interfaces
     {
         Task<List<Stock>> GetUserPortfolio(AppUser user);
         Task<Portfolio> CreateAsync(Portfolio portfolio);
+        Task<Portfolio> DeletePortfolio(AppUser appUser, string symbol);
     }
 }
