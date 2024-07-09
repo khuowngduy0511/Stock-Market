1af3bb30bc2a0e9773fc245620a886826433f91a
Tran Ha Khuong Duy <khuongduy.tech@gmail.com>
2024-07-09 08:56:15 +0700

    config: One-To-One

--- web-api-example/Models/Comment.cs
+++ web-api-example/Models/Comment.cs
@@ -16,6 +16,8 @@ namespace web_api_examlpe.Models
         public DateTime CreatedOn { get; set; } = DateTime.Now;
         public int? StockId { get; set; }
         public Stock? Stock { get; set; }
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
 
     }
 }
